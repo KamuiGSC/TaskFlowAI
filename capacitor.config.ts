b0ab33526a9e0b1cf52ad2f650a1891cc4e0d2c2
Gabriel Soto <gabisotoco@gmail.com>
2024-10-24 12:48:16 -0600

    Fisrt commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.taskflowai',
+  appName: 'taskflowai',
+  webDir: 'www/browser'
+};
+
+export default config;
